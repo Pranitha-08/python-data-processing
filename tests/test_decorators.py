b62e3932d6251eeb66eefd90e520339c4e7b86fa
Pranitha Radharam <radharampranitha48@gmail.com>
2026-09-05 17:52:06 +0530

    Add measure_time decorator to sample_function

--- tests/test_decorators.py
+++ tests/test_decorators.py
@@ -1,1 +1,12 @@
+from de_utils.decorators import measure_time
 
+
+@measure_time
+def sample_function():
+    return "success"
+
+
+def test_measure_time():
+    result = sample_function()
+
+    assert result == "success"
