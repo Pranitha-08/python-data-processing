8c3f881b335f3438c368a14fb63be653cb4f213e
Pranitha Radharam <radharampranitha48@gmail.com>
2026-09-05 17:48:25 +0530

    Implement logger utility function
    Added a logger utility function to create and configure loggers.

--- de_utils/logger.py
+++ de_utils/logger.py
@@ -1,1 +1,20 @@
+import logging
 
+
+def get_logger(name: str) -> logging.Logger:
+    """
+    Create and return a logger for the application.
+    """
+    logger = logging.getLogger(name)
+
+    if not logger.handlers:
+        handler = logging.StreamHandler()
+        formatter = logging.Formatter(
+            "%(asctime)s - %(name)s - %(levelname)s - %(message)s"
+        )
+        handler.setFormatter(formatter)
+        logger.addHandler(handler)
+
+    logger.setLevel(logging.INFO)
+
+    return logger
